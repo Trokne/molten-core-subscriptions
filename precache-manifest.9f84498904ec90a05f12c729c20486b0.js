80d446ddc0b9ea167d35b43a2245ee2ed918b765
Mikhail Vshivkov <m.vshivkov@xsolla.com>
2019-12-26 17:03:47 +0500

    Updates

---
+++ precache-manifest.9f84498904ec90a05f12c729c20486b0.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2d19fb292fab6b74796fa5fb23e518b0",
+    "url": "/molten-core-subscriptions/index.html"
+  },
+  {
+    "revision": "28f84c6f05bb3c9a2ad4",
+    "url": "/molten-core-subscriptions/static/css/main.37056859.chunk.css"
+  },
+  {
+    "revision": "a74c2a67ee75da1a8769",
+    "url": "/molten-core-subscriptions/static/js/2.41519b57.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/molten-core-subscriptions/static/js/2.41519b57.chunk.js.LICENSE"
+  },
+  {
+    "revision": "28f84c6f05bb3c9a2ad4",
+    "url": "/molten-core-subscriptions/static/js/main.17cbeacf.chunk.js"
+  },
+  {
+    "revision": "8ca11bae54e422c33998",
+    "url": "/molten-core-subscriptions/static/js/runtime-main.b558f559.js"
+  },
+  {
+    "revision": "7f88441c4ff9e12d8977d859d1fe52ca",
+    "url": "/molten-core-subscriptions/static/media/background.7f88441c.jpg"
+  }
+]);
